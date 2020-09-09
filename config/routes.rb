6e468baeaa0e125271e00fd37d63316247f55162
oloomoses <olooine@gmail.com>
2020-09-09 23:48:24 +0300

    create user model login and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root 'sessions#new'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login',  to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
 end
