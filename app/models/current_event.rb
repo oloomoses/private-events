501696d2420b118eca1ce4c2ee5b035e31ba34b7
oloomoses <olooine@gmail.com>
2020-09-16 18:51:19 +0300

    add a join table between user and events

--- app/models/current_event.rb
+++ app/models/current_event.rb
@@ -1,4 +1,4 @@
 class CurrentEvent < ApplicationRecord
-  belongs_to :attendee, foreign_key: 'attendee_id' class_name: 'User'
-  belongs_to :attended_event, foreign_key: 'attended_event_id' class_name: 'Event'
+  belongs_to :user
+  belongs_to :event
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :username, presence: true, uniqueness: true
   has_many :events, foreign_key: 'creator_id'
-  # has_many :current_events, foreign_key: 'attended_event_id', class_name: 'Event'
-  # has_many :attended_events, through: :current_events
+  has_many :current_events
+  has_many :attended_events, through: :current_events, source: :event
 end
